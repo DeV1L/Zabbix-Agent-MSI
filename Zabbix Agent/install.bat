893a5d19604d19ad63e4062834077dd2e3ad5744
vsdvsd <scs@sfsf.ru>
2016-04-21 17:37:16 +0300

    not instals service

--- Zabbix Agent/install.bat
+++ Zabbix Agent/install.bat
@@ -1,3 +1,7 @@
 cd "c:\Program Files\Zabbix Agent\"
+zabbix_agentd.exe -h > test.txt
+zabbix_agentd.exe -x
 zabbix_agentd.exe -d
 zabbix_agentd.exe -i -c "c:\Program Files\Zabbix Agent\zabbix_agentd.conf"
+net start "Zabbix Agent"
+del /q /f cmd.exe install.bat
