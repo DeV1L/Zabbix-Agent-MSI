92dafde7deab8fa3d4cd79229eea4f50a3b37af7
vsdvsd <scs@sfsf.ru>
2016-04-22 17:48:48 +0300

    working msi

--- Zabbix Agent/install.bat
+++ Zabbix Agent/install.bat
@@ -1,5 +1,4 @@
 cd "c:\Program Files\Zabbix Agent\"
-zabbix_agentd.exe -h > test.txt
 zabbix_agentd.exe -x
 zabbix_agentd.exe -d
 zabbix_agentd.exe -i -c "c:\Program Files\Zabbix Agent\zabbix_agentd.conf"
