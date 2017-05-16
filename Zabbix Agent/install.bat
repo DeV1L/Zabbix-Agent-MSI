39836956215bdb1302d66bd58e4c85e70da35e1e
y.savchenko <y.savchenko@arkadium.com>
2017-05-16 20:53:47 +0300

    Make three packages

--- Zabbix Agent/install.bat
+++ Zabbix Agent/install.bat
@@ -1,6 +1,7 @@
 cd "c:\Program Files\Zabbix Agent\"
 zabbix_agentd.exe -x
 zabbix_agentd.exe -d
+type NUL >> additional.conf
 zabbix_agentd.exe -i -c "c:\Program Files\Zabbix Agent\zabbix_agentd.conf"
 net start "Zabbix Agent"
-del /q /f cmd.exe install.bat
+
